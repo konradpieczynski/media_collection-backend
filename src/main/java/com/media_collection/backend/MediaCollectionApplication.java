4bcaac03f5ba57f700e81200d6cb5be13212870d
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-07-30 20:09:38 +0200

    Initial

---
+++ src/main/java/com/media_collection/backend/MediaCollectionApplication.java
@@ -0,0 +1,17 @@
+package com.media_collection.backend;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@EnableScheduling
+@OpenAPIDefinition
+@SpringBootApplication
+public class MediaCollectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaCollectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/media_collection/backend/MediaCollectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.media_collection.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediaCollectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
