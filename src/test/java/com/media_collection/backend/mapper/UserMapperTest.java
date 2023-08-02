6eba3d62ce0f2bd1fce29ae1c9bf7b84995777de
Konrad Pieczynski <konrad.pieczynski@gmail.com>
2023-08-02 18:58:33 +0200

    User mapper

--- src/test/java/com/media_collection/backend/mapper/UserMapperTest.java
+++ src/test/java/com/media_collection/backend/mapper/UserMapperTest.java
@@ -1,10 +1,15 @@
 package com.media_collection.backend.mapper;
 
+import com.media_collection.backend.domain.Suggestions;
+import com.media_collection.backend.domain.SuggestionsFactory;
 import com.media_collection.backend.domain.User;
 import com.media_collection.backend.domain.UserDto;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 @SpringBootTest
@@ -12,22 +17,64 @@ class UserMapperTest {
 
     @Autowired
     UserMapper userMapper;
+    @Autowired
+    SuggestionsFactory suggestionsFactory;
     @Test
     void mapToUser() {
-        UserDto userDto = new UserDto(1L, "title", "content");
+        //GIVEN
+        UserDto userDto = new UserDto(1L,
+                "Test user",
+                suggestionsFactory.makeSuggestions("songs"),
+                new ArrayList<>(),
+                new ArrayList<>());
+
         //WHEN
         User user = userMapper.mapToUser(userDto);
         //THEN
-        assertEquals(1L, user.getId());
-        assertEquals("title", user.getTitle());
-        assertEquals("content", user.getContent());
+        assertEquals(1L, user.getUserId());
+        assertEquals("songs", user.getSuggestions().getValue());
+        assertEquals("Test user", user.getUserName());
+        assertEquals(0, user.getMovieCollectionList().size());
+        assertEquals(0, user.getSongCollectionList().size());
     }
 
     @Test
     void mapToUserDto() {
+        //GIVEN
+        User user = new User(1L,
+                "Test UserDto",
+                Suggestions.SONGS,
+                new ArrayList<>(),
+                new ArrayList<>());
+
+        //WHEN
+        UserDto userDto = userMapper.mapToUserDto(user);
+        //THEN
+        assertEquals(1L, userDto.getUserId());
+        assertEquals("songs", userDto.getSuggestions().getType());
+        assertEquals("Test UserDto", userDto.getUserName());
+        assertEquals(0, userDto.getMovieCollectionList().size());
+        assertEquals(0, userDto.getSongCollectionList().size());
     }
 
     @Test
     void mapToUserDtoList() {
+        //GIVEN
+        User user1 = new User();
+        user1.setUserId(1L);
+        user1.setUserName("Test user");
+        user1.setSuggestions(Suggestions.MOVIES);
+        user1.setMovieCollectionList(new ArrayList<>());
+        user1.setSongCollectionList(new ArrayList<>());
+        User user2 = new User();
+        user2.setUserId(2L);
+        user2.setUserName("Test user2");
+        user2.setSuggestions(Suggestions.MOVIES);
+        user2.setMovieCollectionList(new ArrayList<>());
+        user2.setSongCollectionList(new ArrayList<>());
+        //WHEN
+        List<UserDto> userDtoList = userMapper.mapToUserDtoList(List.of(user1,user2));
+        //THEN
+        assertEquals(2, userDtoList.size());
     }
 }
